b396e860c342f1ed61160cfa89048302ed879fc0
praveennkumar007 <128917280+praveennkumar007@users.noreply.github.com>
2023-04-22 11:08:54 -0500

    main
    Completed

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -6,10 +6,16 @@ def logstamp_to_datetime(datestr):
     """
     Your docstring here.  Replace the pass keyword below with your implementation.
     """
-    pass
+    try:
+        # Try to parse the date string as-is
+        return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
+    except ValueError:
+        # If parsing fails, try to add microseconds and parse again
+        return datetime.strptime(datestr + '.000000', '%Y-%m-%dT%H:%M:%S.%f')
+
 
 
 # >>>> The code below will call your function and print the results
-if __name__ == "__main__":
+if _name_ == "_main_":
     test_date = '2022-12-01T01:02:03'
     print(f'{logstamp_to_datetime(test_date)=}')
